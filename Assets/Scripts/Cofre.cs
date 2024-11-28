291c214588bc1c899437ae580ed47c2e361cf4c9
Victendo <victormm2003@gmail.com>
2024-11-28 21:17:44 +0100

    28 / 11

---
+++ Assets/Scripts/Cofre.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cofre : MonoBehaviour
+{
+    private Outline outline;
+
+    [SerializeField] private Texture2D cursorInteraccion;
+    [SerializeField] private Texture2D cursorPorDefecto;
+    // Start is called before the first frame update
+
+    private void Awake()
+    {
+        outline = GetComponent<Outline>();
+    }
+
+
+    private void OnMouseEnter()
+    {
+        Cursor.SetCursor(cursorInteraccion, Vector2.zero, CursorMode.Auto);
+        outline.enabled = true;
+    }
+
+
+
+    private void OnMouseExit()
+    {
+        Cursor.SetCursor(cursorPorDefecto, Vector2.zero, CursorMode.Auto);
+        outline.enabled = false;
+    }
+
+
+}
