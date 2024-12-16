3f8d2a1c3808ecbb35c59a956bdfc7597867548f
Victendo <victormm2003@gmail.com>
2024-12-16 18:21:58 +0100

    16 / 12

--- Assets/Scripts/Enemigo.cs
+++ Assets/Scripts/Enemigo.cs
@@ -6,39 +6,7 @@ using UnityEngine.AI;
 
 public class Enemigo : MonoBehaviour
 {
-    [SerializeField] private Transform ruta;
-    private NavMeshAgent agent;
-
-    List<Vector3> listadoPuntos = new List<Vector3>();
-    // Start is called before the first frame update
-    private Vector3 destinoActual;
-
-    private void Awake()
-    {
-        agent = GetComponent<NavMeshAgent>();
-
-
-        foreach (Transform punto in ruta)
-        {
-            listadoPuntos.Add(punto.position);
-        }
-
-        CalcularDestino();
-    }
-    void Start()
-    {
-        StartCoroutine(PatrullarYEsperar());
-    }
-
-    private IEnumerator PatrullarYEsperar()
-    {
-        agent.SetDestination(destinoActual);
-        yield return null;
-    }
-    private void CalcularDestino()
-    {
-        destinoActual = listadoPuntos[0];
-    }
+    
 }
 
 
---
+++ Assets/Scripts/SistemaBlendTree.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class SistemaBlendTree : MonoBehaviour
+{
+    [SerializeField] private NavMeshAgent agent;
+
+    private Animator anim;
+    
+    // Start is called before the first frame update
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        anim.SetFloat("velocity", agent.velocity.magnitude / agent.speed);
+    }
+}
